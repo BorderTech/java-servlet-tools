634fca6988799ab3dd9373fbae619219e9b3893e
Jonathan Austin <jonoa4n@gmail.com>
2022-03-14 17:57:15 +1000

    Refactor var arg constructors. Closes #2

--- src/main/java/com/github/bordertech/taskmaster/servlet/combo/AbstractComboServletListener.java
+++ src/main/java/com/github/bordertech/taskmaster/servlet/combo/AbstractComboServletListener.java
@@ -1,10 +1,13 @@
 package com.github.bordertech.taskmaster.servlet.combo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -13,6 +16,12 @@ import org.apache.commons.logging.LogFactory;
  * <p>
  * This allows multiple ServletListeners to be easily annotated in one class instead of multiple entries in a web.xml.
  * </p>
+ * <p>
+ * The class is abstract as projects are expected to extend this class and annotate it with the {@link WebListener} annotation.
+ * </p>
+ *
+ * @see WebListener
+ * @see ServletContextListener
  */
 public abstract class AbstractComboServletListener implements ServletContextListener {
 
@@ -21,16 +30,34 @@ public abstract class AbstractComboServletListener implements ServletContextList
 	private final List<ServletContextListener> listeners;
 
 	/**
-	 * @param listeners the group of listeners to combine into a single listener
+	 * @param listener the first listener to combine
+	 * @param listeners the other listeners to combine into a single listener
 	 */
-	public AbstractComboServletListener(final ServletContextListener... listeners) {
-		this(Arrays.asList(listeners));
+	protected AbstractComboServletListener(final ServletContextListener listener, final ServletContextListener... listeners) {
+		if (listener == null) {
+			throw new IllegalArgumentException("Listener cannot be null");
+		}
+		boolean hasNull = Arrays.stream(listeners).anyMatch(Objects::isNull);
+		if (hasNull) {
+			throw new IllegalArgumentException("Listeners cannot contain a null listener");
+		}
+		List<ServletContextListener> backing = new ArrayList<>();
+		backing.add(listener);
+		backing.addAll(Arrays.asList(listeners));
+		this.listeners = Collections.unmodifiableList(backing);
 	}
 
 	/**
 	 * @param listeners the group of listeners to combine into a single listener
 	 */
-	public AbstractComboServletListener(final List<ServletContextListener> listeners) {
+	protected AbstractComboServletListener(final List<ServletContextListener> listeners) {
+		if (listeners == null || listeners.isEmpty()) {
+			throw new IllegalArgumentException("List of listeners cannot be null or empty");
+		}
+		boolean hasNull = listeners.stream().anyMatch(Objects::isNull);
+		if (hasNull) {
+			throw new IllegalArgumentException("Listeners list cannot contain a null listener");
+		}
 		this.listeners = Collections.unmodifiableList(listeners);
 	}
 
@@ -60,7 +87,7 @@ public abstract class AbstractComboServletListener implements ServletContextList
 			try {
 				listener.contextDestroyed(sce);
 			} catch (Exception e) {
-				LOGGER.error("Error calling destoryed servlet context listener [" + listener.getClass().getName() + "]. "
+				LOGGER.error("Error calling destroyed servlet context listener [" + listener.getClass().getName() + "]. "
 						+ e.getMessage(), e);
 			}
 		}
